83aeba3cd0292cbf49ccce113faf3e1dcbdb70a7
David <2018371056@uteq.edu.mx>
2021-09-24 23:25:32 -0500

    Commit Inicial

---
+++ App2/App2/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace App2.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisedPropertyChanged (string nombrePropiedad)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nombrePropiedad));
+        }
+    }
+}
---
+++ App2/App2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace App2.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string _nombre;
+        public MainPageViewModel()
+        {
+            _nombre = "Hola";
+        }
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (string.Equals(_nombre, value)) return;
+
+                _nombre = value;
+                RaisedPropertyChanged(nameof(Nombre));
+
+                RaisedPropertyChanged(nameof(SinUpdate));
+
+            }
+
+        }
+        public string SinUpdate
+        {
+            get => _nombre;
+        }
+    }
+}
